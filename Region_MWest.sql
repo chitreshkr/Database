94c4ba0f7c69bc679b06bd04c01092341cd7b843
Chitresh Kumar <8807772+chitreshkr@users.noreply.github.com>
2019-10-24 17:07:36 -0500

    Create Region_MWest.sql

---
+++ Region_MWest.sql
@@ -0,0 +1,7 @@
+select reg.name as "Region",sr.name as "Sales Person",acc.name as "Account Name"
+from accounts acc
+JOIN sales_reps sr
+ON acc.sales_rep_id = sr.id
+JOIN region reg
+ON sr.region_id = reg.id
+where reg.name = 'Midwest'
